0e70e794d9e91939138c4e9d82aaba245500ed47
MSot22 <marinasoteriou412@gmail.com>
2025-04-21 14:44:58 +0300

    Delete update_manager.py

--- update_manager.py
+++
@@ -1,8 +0,0 @@
-from firmware import Firmware
-
-class UpdateManager:
-    def __init__(self):
-        pass
-
-    def generate_valid_firmware(self, version, payload):
-        return Firmware(version=version, payload=payload, signed=True)
